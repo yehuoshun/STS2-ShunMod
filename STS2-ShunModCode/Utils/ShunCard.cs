076cea75c92532659e1775e320a5a826513973af
yehuoshun <2431038086@qq.com>
2026-05-01 01:44:24 +0800

    ShunCard 最小化：仅含 SuperApotheosis 实际使用的方法

--- STS2-ShunModCode/Utils/ShunCard.cs
+++ STS2-ShunModCode/Utils/ShunCard.cs
@@ -1,5 +1,3 @@
-using System.Text.RegularExpressions;
-using Godot;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
@@ -7,10 +5,42 @@ using MegaCrit.Sts2.Core.Models;
 
 namespace STS2_ShunMod.Utils;
 
-/// <summary>
-/// ShunMod 卡牌基类。
-/// </summary>
-public abstract partial class ShunCard : CardModel
+public abstract class ShunCard : CardModel
 {
-   
+    private readonly List<CardKeyword> _keywords = [];
+    private readonly List<Func<CardModel, IHoverTip>> _hoverTips = [];
+
+    private int? _costUpgrade;
+
+    protected ShunCard(int baseCost, CardType type, CardRarity rarity, TargetType target)
+        : base(baseCost, type, rarity, target)
+    {
+    }
+
+    protected sealed override IEnumerable<DynamicVar> CanonicalVars => [];
+    public sealed override IEnumerable<CardKeyword> CanonicalKeywords => _keywords;
+    protected sealed override HashSet<CardTag> CanonicalTags => [];
+    protected sealed override IEnumerable<IHoverTip> ExtraHoverTips =>
+        _hoverTips.Select(t => t(this));
+
+    protected override void OnUpgrade()
+    {
+        if (_costUpgrade.HasValue)
+            EnergyCost.UpgradeBy(_costUpgrade.Value);
+    }
+
+    protected void WithKeywords(params CardKeyword[] keywords)
+    {
+        _keywords.AddRange(keywords);
+    }
+
+    protected void WithTip(CardKeyword keyword)
+    {
+        _hoverTips.Add(_ => HoverTipFactory.FromKeyword(keyword));
+    }
+
+    protected void WithCostUpgradeBy(int amount)
+    {
+        _costUpgrade = amount;
+    }
 }
