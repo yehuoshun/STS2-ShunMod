425c748ab3fd51ee63396a1428d5e8fa0b03ede9
yehuoshun <2431038086@qq.com>
2026-05-01 02:29:27 +0800

    fix: 添加 PlayerModel using

--- STS2-ShunModCode/Events/RelicExchangeEvent.cs
+++ STS2-ShunModCode/Events/RelicExchangeEvent.cs
@@ -1,5 +1,6 @@
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Events;
 using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Models;
