f550210370da87a4576aa0d3c2796cc2b17395ca
yehuoshun <2431038086@qq.com>
2026-05-01 00:49:03 +0800

    硬化贝壳不硬化

---
+++ STS2-ShunModCode/patch/HardenedShellPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using System;
+using MegaCrit.Sts2.Core.Logging;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace ShunMod;
+
+[HarmonyPatch(typeof(HardenedShellPower), "ModifyHpLostBeforeOstyLate")]
+public static class HardenedShellPowerPatch
+{
+    static void Postfix(HardenedShellPower __instance, Creature target, decimal amount, ValueProp props, Creature? dealer, CardModel? cardSource, ref decimal __result)
+    {
+        __result = amount;
+    }
+}
