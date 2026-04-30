bddc215b9202f5f5e6509a576a415b5d8efd4aa3
yehuoshun <2431038086@qq.com>
2026-05-01 01:40:23 +0800

    修复 WithUpgrade：添加 SpireField + DynamicVarExtensions 扩展

--- STS2-ShunModCode/Utils/ShunCard.cs
+++ STS2-ShunModCode/Utils/ShunCard.cs
@@ -5,6 +5,7 @@ using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
+using STS2_ShunMod.Extensions;
 
 namespace STS2_ShunMod.Utils;
 
