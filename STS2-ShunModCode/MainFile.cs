f9f8d2b876e0de187cb4c5bb1be7521ddef6f48b
yehuoshun <2431038086@qq.com>
2026-05-01 15:34:16 +0800

    修复卡牌注册问题

--- STS2-ShunModCode/MainFile.cs
+++ STS2-ShunModCode/MainFile.cs
@@ -8,35 +8,16 @@ using STS2_ShunMod.Patches;
 
 namespace STS2_ShunMod;
 
+[ModInitializer(nameof(Initialize))]
 public static class MainFile
 {
     public const string ModId = "STS2_ShunMod";
 
     private static readonly Harmony Harmony = new(ModId);
 
-    static MainFile()
+    public static void Initialize()
     {
         Harmony.PatchAll();
-    }
-}
-
-/// <summary>
-/// 在主菜单加载时注册自定义内容（卡牌 + 事件）。
-/// </summary>
-[HarmonyPatch(typeof(NMainMenu), nameof(NMainMenu._Ready))]
-public static class ContentRegistrationPatch
-{
-    private static bool _registered;
-
-    static void Postfix()
-    {
-        if (_registered) return;
-        _registered = true;
-
-        // 注册卡牌
         ModHelper.AddModelToPool(typeof(ColorlessCardPool), typeof(SuperApotheosis));
-
-        // 注册事件
-        EventRegistry.Register(new RelicExchangeEvent());
     }
 }
