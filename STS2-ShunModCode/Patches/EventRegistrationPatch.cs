f4a105350e499b4ec6758392941f598d7bf3653a
yehuoshun <2431038086@qq.com>
2026-05-16 08:59:54 +0800

    修复事件注册：Harmony patch Overgrowth.AllEvents getter

--- STS2-ShunModCode/Patches/EventRegistrationPatch.cs
+++ STS2-ShunModCode/Patches/EventRegistrationPatch.cs
@@ -2,24 +2,26 @@ using System.Collections.Generic;
 using System.Linq;
 using HarmonyLib;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Rooms;
 using STS2_ShunMod.Events;
 
 namespace STS2_ShunMod.Patches;
 
 /// <summary>
-/// 将 ShunModRelicExchange 事件注册到游戏事件列表。
+/// 将遗物交易所事件注入到游戏事件列表。
+/// 修改 Overgrowth.AllEvents（第一章地图），
+/// 如需出现在其他章节，同样 patch City / Beyond。
 /// </summary>
-[HarmonyPatch]
+[HarmonyPatch(typeof(Overgrowth), "AllEvents", MethodType.Getter)]
 public static class EventRegistration_RelicExchange
 {
-    static IEnumerable<EventModel> Postfix(IEnumerable<EventModel> events)
+    static void Postfix(ref IReadOnlyList<EventModel> __result)
     {
-        foreach (var e in events)
-            yield return e;
-
+        var list = __result.ToList();
         var relicExchange = ModelDb.GetByIdOrNull<EventModel>(
             ModelDb.GetId(typeof(ShunModRelicExchange)));
         if (relicExchange != null)
-            yield return relicExchange;
+            list.Add(relicExchange);
+        __result = list;
     }
 }
