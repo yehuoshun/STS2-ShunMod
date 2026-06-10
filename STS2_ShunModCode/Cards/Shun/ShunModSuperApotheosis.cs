8e11afeaf76717bf400f4a19e7d018f7ff2fe121
yehuoshun <2431038086@qq.com>
2026-06-10 23:07:50 +0800

    卡牌图片名对齐类名：superapotheosis → shun_mod_super_apotheosis

--- STS2_ShunModCode/Cards/Shun/ShunModSuperApotheosis.cs
+++ STS2_ShunModCode/Cards/Shun/ShunModSuperApotheosis.cs
@@ -15,7 +15,7 @@ namespace STS2ShunMod.STS2_ShunModCode.Cards.Shun;
 [CardPool(typeof(ColorlessCardPool))]
 public class ShunModSuperApotheosis : CardModel
 {
-    private const string Portrait = "res://STS2_ShunMod/images/cards/shunCards/colorless/superapotheosis.png";
+    private const string Portrait = "res://STS2_ShunMod/images/cards/shunCards/colorless/shun_mod_super_apotheosis.png";
 
     public ShunModSuperApotheosis() : base(2, CardType.Skill, CardRarity.Rare, TargetType.Self) { }
 
