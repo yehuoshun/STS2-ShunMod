d83dda4ebef322f40afd03f6efec370717d78ef6
yehuoshun <2431038086@qq.com>
2026-07-17 17:25:53 +0800

    清楚提示

--- ShunMod.Tweaks/Patches/RestSite/EnchantRestSitePatch.cs
+++ ShunMod.Tweaks/Patches/RestSite/EnchantRestSitePatch.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using HarmonyLib;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.RestSite;
@@ -9,8 +9,11 @@ namespace ShunMod.Tweaks.Patches.RestSite;
 ///     休息处附魔选项 — Postfix 注入 RestSiteOption.Generate，永远多一个「附魔」选项。
 /// </summary>
 [HarmonyPatch(typeof(RestSiteOption), nameof(RestSiteOption.Generate))]
+[SuppressMessage("ReSharper", "UnusedType.Global")]
 public static class EnchantRestSitePatch
 {
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
     static void Postfix(Player player, ref List<RestSiteOption> __result)
     {
         __result.Add(new EnchantRestSiteOption(player));
