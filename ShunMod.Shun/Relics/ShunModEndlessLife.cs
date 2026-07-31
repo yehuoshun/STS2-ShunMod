cd21dcd03357bb57f757514a2f1956b848893eec
yehuoshun <2431038086@qq.com>
2026-08-01 05:25:22 +0800

    清理using

--- ShunMod.Shun/Relics/ShunModEndlessLife.cs
+++ ShunMod.Shun/Relics/ShunModEndlessLife.cs
@@ -1,16 +1,11 @@
-using System.Linq;
 using MegaCrit.Sts2.Core.CardSelection;
 using MegaCrit.Sts2.Core.Commands;
-using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Relics;
-using MegaCrit.Sts2.Core.Factories;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.RelicPools;
-using MegaCrit.Sts2.Core.Rooms;
 using MegaCrit.Sts2.Core.Runs;
 using ShunMod.Core.Core.Registry;
 using ShunMod.Shun.Base;
