6386d3329cd3eb4601bbe8273f155d584813492e
黑奴 <@localhost>
2026-08-01 05:36:33 +0800

    fix: 移除 endlessLife.Owner 冗余 null 检查

--- ShunMod.Shun/Patches/RelicRightClickPatch.cs
+++ ShunMod.Shun/Patches/RelicRightClickPatch.cs
@@ -53,9 +53,6 @@ internal static class RelicRightClickPatch
         if (relicNode.Model is not ShunModEndlessLife endlessLife)
             return;
 
-        if (endlessLife.Owner == null)
-            return;
-
         // 战斗中进行
         if (!CombatManager.Instance.IsInProgress
             || CombatManager.Instance.IsEnding)
